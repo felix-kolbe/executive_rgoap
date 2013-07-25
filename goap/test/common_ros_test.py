bc91298b0b8f86a30e685a111a9e939fc46297a2
Felix Kolbe <felix.kolbe@informatik.haw-hamburg.de>
2013-07-25 15:11:18 +0200

    Proceed implementing ROSTopicCondition

--- goap/test/common_ros_test.py
+++ goap/test/common_ros_test.py
@@ -29,52 +29,42 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import roslib; roslib.load_manifest('goap')
+import unittest
+
+from goap import common_ros
+
+from std_msgs.msg import Bool
 import rospy
 
-import rostopic
+class TestRTC(unittest.TestCase):
 
-from std_msgs.msg import Empty
+    topic = '/testbool'
 
-from goap import Action, Condition, Precondition, Effect
-
-
-class ResetBumperAction(Action):
-
-    def __init__(self):
-        Action.__init__(self, [Precondition(Condition.get('robot.bumpered'), True)],
-                            [Effect(Condition.get('robot.bumpered'), False)])
-
-    def run(self):
-        rospy.Publisher('/bumper_reset', Empty).publish()
+    def setUp(self):
+        self.rtc = common_ros.ROSTopicCondition(TestRTC.topic, '/data', 'topic.testbool')
+        rospy.init_node(self.__class__.__name__)
 
 
-class ROSTopicCondition(Condition):
-
-    def __init__(self, topic, field, state_name):
-        Condition.__init__(self, state_name)
-        self._topic = topic
-        self._field = field
-
-        topic_type, real_topic, msg_eval = rostopic.get_topic_class(topic)
-        print 'topic_type, real_topic, msg_eval = ', topic_type, real_topic, msg_eval
-        self._subscriber = rospy.Subscriber(topic, topic_type, self._callback)
-        self._msgeval = rostopic.msgevalgen(field)
-        self._value = None
-
-        self._publisher = rospy.Publisher(topic, topic_type)
+    def tearDown(self):
+        pass
 
 
-    def __repr__(self):
-        return '<ROSTopicCondition: topic=%s field=%s>' % self._topic, self._field
+    def testName(self):
+        publisher = rospy.Publisher(TestRTC.topic, Bool, latch=True)
 
-    def _callback(self, msg):
-        self._value = self._msgeval(msg)
-        print 'callback with: ', self._value
+        self.assertIsNone(self.rtc.get_value(None), 'New topic cond should have value None.')
 
-    def get_value(self, worldstate):
-        return self._value
+        print 'publishing..'
 
-    def set_value(self, worldstate):
-        # TODO
-        self._publisher.publish(None)
+        publisher.publish(True)
+        rospy.sleep(1)
+        self.assertTrue(self.rtc.get_value(None), 'Now topic cond should have value True.')
+
+        publisher.publish(False)
+        rospy.sleep(1)
+        self.assertFalse(self.rtc.get_value(None), 'Now topic cond should have value False.')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
