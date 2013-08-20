777b6fd9b4baa661669c534c0d0b7699f0e524ba
Felix Kolbe <felix.kolbe@informatik.haw-hamburg.de>
2013-08-20 16:10:56 +0200

    Add heuristic distance calculation to Node and use distance in Planner
    Extend GOAPTestBumpered to provoke non-numeric comparisons
    
    Remove module planning_forward

--- goap/test/GOAPTestBumpered.py
+++ goap/test/GOAPTestBumpered.py
@@ -75,6 +75,7 @@ if __name__ == "__main__":
                     'robot.bumpered', '/bumper', ScitosG5Bumper, '/motor_stop'))
     Condition.add(MemoryCondition(runner.memory, 'reminded_myself'))
 
+    runner.memory.set_value('memory.reminded_myself', 333)
 
     print 'Waiting to let conditions represent reality...'
     print 'Remember to start topic publishers so conditions make sense instead of None!'
@@ -84,9 +85,11 @@ if __name__ == "__main__":
 
     runner.actionbag.add(ResetBumperAction())
     runner.actionbag.add(MoveBaseAction())
+    runner.actionbag.add(MemoryChangeVarAction(runner.memory, 'reminded_myself', 333, 555))
 
 
-    goal = Goal([Precondition(Condition.get('robot.pose'), calc_Pose(1, 0, 0))])
+    goal = Goal([Precondition(Condition.get('robot.pose'), calc_Pose(1, 0, 0)),
+                 Precondition(Condition.get('memory.reminded_myself'), 555)])
 
     start_node = runner.update_and_plan(goal, introspection=True)
 
@@ -95,7 +98,7 @@ if __name__ == "__main__":
     rospy.sleep(10)
 
     if start_node is None:
-        print 'No plan found! Check you ROS graph!'
+        print 'No plan found! Check your ROS graph!'
     else:
         PlanExecutor().execute(start_node)
 
