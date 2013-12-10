3cb490aafd68aba4b263cd6bbd767ba7f07bcec6
Felix Kolbe <felix.kolbe@informatik.haw-hamburg.de>
2013-12-10 15:02:15 +0100

    Add ROSTopicAction as specialization of Action Extend common_ros_test accordingly

--- rgoap_ros/src/rgoap_ros/__init__.py
+++ rgoap_ros/src/rgoap_ros/__init__.py
@@ -29,7 +29,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-from common_ros import ROSTopicCondition
+from common_ros import ROSTopicCondition, ROSTopicAction
 
 from introspection import Introspector
 
